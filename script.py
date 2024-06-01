4822110a6897a07a735f99f7466c50bf739c9dd6
ftnext <takuyafjp+develop@gmail.com>
2024-06-01 12:10:54 +0900

    [feat] User can specify verbosity

--- script.py
+++ script.py
@@ -1,9 +1,15 @@
 import logging
+import sys
 
 from awesomelib import awesome
 
+if len(sys.argv) > 1 and sys.argv[1] == "-v":
+    log_level = logging.DEBUG
+else:
+    log_level = logging.WARNING
+
 logging.basicConfig(
-    level=logging.WARNING,
+    level=log_level,
     format="%(asctime)s | %(levelname)s | %(name)s:%(funcName)s:%(lineno)d - %(message)s",
 )
 
