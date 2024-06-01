c990d4b6eb1d3c3e8108d166c062fbf9cf79455c
ftnext <takuyafjp+develop@gmail.com>
2024-06-01 12:05:56 +0900

    [feat] Logging in library
    ref: https://docs.python.org/ja/3/howto/logging.html#library-config

--- libs/awesomelib/src/awesomelib/__init__.py
+++ libs/awesomelib/src/awesomelib/__init__.py
@@ -1,2 +1,10 @@
+import logging
+
+logger = logging.getLogger(__name__)
+logger.addHandler(logging.NullHandler())
+
+
 def awesome() -> None:
+    logger.info("想定通り")
+    logger.warning("ちょっとヤバいよ")
     print("Awesome ✨")
