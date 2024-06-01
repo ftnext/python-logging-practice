d1adf71f31801e9e47c82be5db3ce7b4610462a1
ftnext <takuyafjp+develop@gmail.com>
2024-06-01 12:08:41 +0900

    [feat] Show logs in user's script

--- script.py
+++ script.py
@@ -1,3 +1,10 @@
+import logging
+
 from awesomelib import awesome
+
+logging.basicConfig(
+    level=logging.WARNING,
+    format="%(asctime)s | %(levelname)s | %(name)s:%(funcName)s:%(lineno)d - %(message)s",
+)
 
 awesome()
