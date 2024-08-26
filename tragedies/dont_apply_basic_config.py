5d303b7452f6fb2f2d0c60a3e4b0a0f814ae8d4c
ftnext <takuyafjp+develop@gmail.com>
2024-08-27 01:39:40 +0900

    [docs] Workaround but root logger in library is wrong

--- tragedies/dont_apply_basic_config.py
+++ tragedies/dont_apply_basic_config.py
@@ -2,10 +2,18 @@ import logging
 
 import antipattern_logging.call_basic_config  # Call basicConfig once
 
+# ref: https://github.com/python/cpython/blob/v3.12.5/Lib/logging/__init__.py#L2097-L2099
+# root_logger = logging.getLogger()
+# for handler in root_logger.handlers[:]:
+#     root_logger.removeHandler(handler)
+#     handler.close()
+# >This function does nothing if the root logger already has handlers configured
+# https://docs.python.org/3/library/logging.html#logging.basicConfig
 logging.basicConfig(
     level=logging.WARNING,
     format="%(asctime)s | %(levelname)s | %(name)s:%(funcName)s:%(lineno)d - %(message)s",
 )
+# root_logger.info("INFO log")  # Not printed
 
 # By library, level=INFO, format=%(levelname)s:%(name)s:%(message)s
 logging.getLogger().warning("ちょっとヤバいよ")
